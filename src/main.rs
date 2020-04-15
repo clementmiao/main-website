3deae487d4758191a8ab73e2589b52e01d6e9ca0
Clement Miao <clementmiao@gmail.com>
2020-04-15 15:07:56 -0400

    move personal website to this

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_files as fs;
+use actix_web::{web, App, HttpServer, HttpResponse, Responder};
+use tera::{Tera, Context};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+
+async fn render_tmpl(data: web::Data<AppData>) -> impl Responder {
+    let ctx = Context::new();
+    let rendered = data.tmpl.render("index.html", &ctx).unwrap();
+    HttpResponse::Ok().body(rendered)
 }
+
+struct AppData {
+    tmpl: Tera
+}
+
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+        let tera =
+            Tera::new(
+                concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*")
+            ).unwrap();
+
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .data(AppData {tmpl: tera})
+            .service(fs::Files::new("/static", ".").show_files_listing())
+            .service(
+                web::resource("/")
+                    .route(web::get().to(render_tmpl))
+            )
     })
     .bind("0.0.0.0:8000")?
     .run()
