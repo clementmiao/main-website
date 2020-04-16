dc63e2c4c1bbed40ca5243dc148b07a57d3d691e
Clement Miao <clementmiao@gmail.com>
2020-04-16 16:44:55 -0400

    scrollIntoView and actions :)

--- static/command_palette.js
+++ static/command_palette.js
@@ -2,46 +2,55 @@ var actions = [{
         id: 0,
         text: 'Delete deal',
         keyBinding: 'a',
+        execute: () => console.log('a'),
     },
     {
         id: 1,
         text: 'Close deal',
         keyBinding: 'b',
+        execute: () => console.log('b'),
     },
     {
         id: 2,
         text: 'Re-open deal',
         keyBinding: 'c',
+        execute: () => console.log('c'),
     },
     {
         id: 3,
         text: 'eat potatoes',
         keyBinding: 'd',
+        execute: () => console.log('d'),
     },
     {
         id: 4,
         text: 'cook potatoes',
         keyBinding: 'e',
+        execute: () => console.log('e'),
     },
     {
         id: 5,
         text: 'buy potatoes',
         keyBinding: 'f',
+        execute: () => console.log('f'),
     },
     {
         id: 6,
         text: 'fly a kite',
         keyBinding: 'g',
+        execute: () => console.log('g'),
     },
     {
         id: 7,
         text: 'buy a bike',
         keyBinding: 'h',
+        execute: () => console.log('h'),
     },
     {
         id: 8,
         text: 'party time',
         keyBinding: 'i',
+        execute: () => console.log('i'),
     },
 ];
 
@@ -56,16 +65,21 @@ var app = new Vue({
     },
     computed: {
         matches: function () {
+            this.current = 0;
             return this.actionsList.filter(obj => {
                 return obj.text.indexOf(this.value) >= 0
             })
         },
-        openSugggestion: function () {
-            return this.value !== '' && this.matches.length !== 0 && this.open === true && this.seen === true
-        },
-
     },
     methods: {
+        scrollActionIntoview: function (index) {
+            const matchId = this.matches[index].id;
+            const selectedNode = this.$refs[`action${matchId}`];
+            scrollIntoView(selectedNode[0], {
+                behavior: 'smooth',
+                scrollMode: 'if-needed'
+            });
+        },
         updateValue: function (value) {
             if (this.open === value) {
                 this.open = true;
@@ -76,18 +90,20 @@ var app = new Vue({
         },
         enter: function () {
             const selectedAction = this.matches[this.current];
-            console.log(selectedAction.text);
+            selectedAction.execute();
             this.open = false;
             this.seen = false;
         },
         up: function () {
             if (this.current > 0) {
-                this.current--
+                this.current--;
+                this.scrollActionIntoview(this.current);
             }
         },
         down: function () {
             if (this.current < this.matches.length - 1) {
-                this.current++
+                this.current++;
+                this.scrollActionIntoview(this.current);
             }
         },
         isActive: function (index) {
