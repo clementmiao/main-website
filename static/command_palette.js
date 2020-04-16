bfccdb1c0ad5e9a4525341caf55e7974dd61a485
Clement Miao <clementmiao@gmail.com>
2020-04-15 23:53:41 -0400

    filtering, keyboard shortcut to open and close palette

--- static/command_palette.js
+++ static/command_palette.js
@@ -1,22 +1,42 @@
 Vue.component('action-item', {
-    props: ['action', 'message'],
-    template: '<li>{{ action.text }} {{ message }}</li>'
+    props: ['action'],
+    template: '<li>{{ action.text }}</li>'
 });
+
+var actions = [
+    { id: 0, text: 'Delete deal' },
+    { id: 1, text: 'Close deal' },
+    { id: 2, text: 'Re-open deal' },
+];
 
 var app = new Vue({ 
     el: '#app',
     data: {
         seen: false,
-        message: 'input a command...',
-        actionList: [
-            { id: 0, text: 'Delete deal' },
-            { id: 1, text: 'Close deal' },
-            { id: 2, text: 'Re-open deal' }
-        ]
+        search: '',
+        actionsList: actions,
     },
-    methods: {
-        showCommandPalette: function () {
-            this.seen = !this.seen
+    watch: {
+        search: function (input) {
+            this.actionsList = actions.filter(action => action.text.toLowerCase().includes(input.toLowerCase()));
         },
-    }
+    },
+     mounted() {
+        this._keyListener = function(e) {
+            if (e.key === " " && (e.ctrlKey || e.metaKey)) {
+                e.preventDefault();
+                this.seen = !this.seen;
+                if (this.seen === true) {
+                    this.$nextTick(function() {
+                        this.$refs.search.focus()
+                    })
+                }
+            }
+        };
+
+        document.addEventListener('keydown', this._keyListener.bind(this));
+    },
+    beforeDestroy() {
+        document.removeEventListener('keydown', this._keyListener);
+    },
 });
